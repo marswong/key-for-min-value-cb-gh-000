eec236eb5b683a843f320a20730df61500c16277
Mars Wong <marswong618@gmail.com>
2018-01-29 02:47:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   if name_hash != {}
     name_hash.each do |key, val|
       if min.nil?
-        min = val
+        min = key
       elsif val <= min
         min = key
       end
